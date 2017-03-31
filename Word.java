1fe4912ea04b4141ad7545bdb187b6ce15d731cd
Kuan-Chi Chen <chen26k@mtholyoke.edu>
2017-03-31 16:29:58 -0400

    Add files via upload

--- Word.java
+++ Word.java
@@ -1,41 +1,75 @@
 
-public class Word  {
-	int count[] = new int[26];  // count of each letter in the word
-	int total;  // number of letters in the word
-	String aword;  // the word
+public class Word {
+	/** the array representation of the word **/
+	int count[] = new int[26]; // count of each letter in the word
+	/**
+	 * the total of the numbers of the letters in the word, thus the length of
+	 * the word
+	 **/
+	int total; // number of letters in the word
+	/** the word itself **/
+	String aword; // the word
 
+	/**
+	 * 
+	 * @param s
+	 */
 	public Word(String s) { // construct an entry from a string
+		// each letter's difference from 'a'
 		int ch;
+		// get the string from the parameter
 		aword = s;
+		// the total is 0 now
 		total = 0;
+		// change all letters to lower case
 		s = s.toLowerCase();
-		for (int i = 'a'; i <= 'z'; i++) count[i-'a'] = 0;
-
-		for (int i = s.length()-1; i >= 0; i--) {
+		// get rid of the first for loop
+		for (int i = 'a'; i <= 'z'; i++)
+			count[i - 'a'] = 0;
+		//
+		for (int i = s.length() - 1; i >= 0; i--) {
+			// calculates the difference between this letter and 'a'
 			ch = s.charAt(i) - 'a';
+			// so if this is an alphabet
 			if (ch >= 0 && ch < 26) {
+				// it will add to total
 				total++;
+				// add to the array that you have one more of this letter
 				count[ch]++;
 			}
 		}
 	}
 
-	public boolean containsLetter(int j){
+	/**
+	 * 
+	 * @param j
+	 * @return
+	 */
+	public boolean containsLetter(int j) {
+		//
 		return count[j] != 0;
 	}
 
-	public int MultiFieldCompare(Word t, int LeastCommonIndex)
-	{
-		if ( (containsLetter(LeastCommonIndex) ) &&  !(t.containsLetter(LeastCommonIndex)) )
+	/**
+	 * Should not be in the Word class, should be put in the Wordlist class
+	 * 
+	 * only being called in anagram
+	 * 
+	 * @param t
+	 * @param LeastCommonIndex
+	 * @return
+	 */
+	public int MultiFieldCompare(Word t, int LeastCommonIndex) {
+		//
+		if ((containsLetter(LeastCommonIndex)) && !(t.containsLetter(LeastCommonIndex)))
 			return 1;
-		
-		if ( !(containsLetter(LeastCommonIndex) ) &&  (t.containsLetter(LeastCommonIndex)) )
+
+		if (!(containsLetter(LeastCommonIndex)) && (t.containsLetter(LeastCommonIndex)))
 			return -1;
-		
-		if ( t.total != total )
+
+		if (t.total != total)
 			return (t.total - total);
-		
+
 		return (aword).compareTo(t.aword);
 	}
 }
-
--- WordList.java
+++ WordList.java
@@ -1,41 +1,61 @@
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class WordList implements UsefulConstants {
+	//
 	static Word[] Dictionary = new Word[MAXWORDS];
-	static int totWords=0;
+	//
+	static int totWords = 0;
 
-	static void ReadDict (String f) {
+	static void ReadDict(String f) {
+		//
 		FileInputStream fis;
 		try {
-			fis = new FileInputStream (f);
-		}
-		catch (FileNotFoundException fnfe) {
+			fis = new FileInputStream(f);
+		} catch (FileNotFoundException fnfe) {
 			e.println("Cannot open the file of words '" + f + "'");
 			throw new RuntimeException();
 		}
-		e.println ("reading dictionary...");
-		
+		e.println("reading dictionary...");
+
 		char buffer[] = new char[MAXWORDLEN];
+
 		String s;
-		int r =0;
-		while (r!=EOF) {
+
+		int r = 0;
+
+		// when r is not EOF
+		while (r != EOF) {
+
 			int i = 0;
+
 			try {
 				// read a word in from the word file
-				while ( (r=fis.read()) != EOF ) {
-					if ( r == '\n' ) break;
+
+				while ((r = fis.read()) != EOF) {
+
+					if (r == '\n')
+						break;
+
 					buffer[i++] = (char) r;
+
 				}
+
 			} catch (IOException ioe) {
+
 				e.println("Cannot read the file of words ");
 				throw new RuntimeException();
+
 			}
-			
-			s = new String(buffer,0,i);
+
+			s = new String(buffer, 0, i);
+
 			Dictionary[totWords] = new Word(s);
+
 			totWords++;
 		}
-		
+
 		e.println("main dictionary has " + totWords + " entries.");
 	}
 }
